2e30c852c1ede16f81ea2030fa37023cf23d2e78
justindlangley <justindlangley@gmail.com>
2015-03-23 15:13:36 -0400

    added comment
    ..

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,10 +12,10 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             //Print string "Hello" and print arguments "Justin" and "Langley"
-            //Console.WriteLine("Hello " + args [0] + " " + args [1]);
+            Console.WriteLine("Hello " + args [0] + " " + args [1]);
             
             //Print arguments Justin and Langley with space in between
-            //Console.WriteLine(args [0] + " " + args [1]);
+            Console.WriteLine(args [0] + " " + args [1]);
 
             //Print arguments in order Langley and Justin with comma separator 
             Console.WriteLine(args [1] + ", " + args [0]);
